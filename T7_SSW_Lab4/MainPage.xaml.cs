2c06567920a6764d73ebfd633acedcdd64586dd4
Maxim Bondarchuk <maksim.bondarchuk@gmail.com>
2015-11-15 14:16:06 +0200

    Back to WSA

--- T7_SSW_Lab4/MainPage.xaml.cs
+++ T7_SSW_Lab4/MainPage.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using Windows.Foundation;
-using Windows.UI.Popups;
+using Windows.UI;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -16,6 +19,21 @@ namespace T7_SSW_Lab4
         private readonly Schedule _schedule = new Schedule();
         private const int TestsCount = 100;
 
+        // Colors for graphs
+        private readonly List<Color> _colors = new List<Color>
+        {
+            Color.FromArgb(100, 153, 0, 0),
+            Color.FromArgb(100, 51, 102, 0),
+            Color.FromArgb(100, 0, 102, 102),
+            Color.FromArgb(100, 0, 51, 102),
+            Color.FromArgb(100, 76, 0, 153),
+            Color.FromArgb(100, 153, 0, 176),
+            Color.FromArgb(100, 32, 32, 32)
+        };
+
+        private int _colorIdx;
+
+
         public MainPage()
         {
             InitializeComponent();
@@ -23,45 +41,61 @@ namespace T7_SSW_Lab4
 
         private void ButtonGenerate_Click(object sender, RoutedEventArgs e)
         {
-            
-            _schedule.GenerateMarix(10, Convert.ToInt16(SliderSize.Value));
-
-            TextBoxMatrix.Text = "";
-            foreach (var row in _schedule.Matrix)
-            {
-                for (var j = 0; j < _schedule.Matrix.Count; j++)
-                    TextBoxMatrix.Text += $"{row[j],4}";
-                TextBoxMatrix.Text += "\n";
-            }
-        }
-
-        private async void ButtonTransform_Click(object sender, RoutedEventArgs e)
-        {
-            _schedule.TransformMatrix();
-            TextBoxTransformed.Text = "";
-            foreach (var row in _schedule.Matrix)
-            {
-                for (var j = 0; j < _schedule.Matrix.Count; j++)
-                    TextBoxTransformed.Text += $"{row[j],4}";
-                TextBoxTransformed.Text += "\n";
-            }
-            var dialog = new MessageDialog(_schedule.CheckConflict().ToString());
-            await dialog.ShowAsync();
-        }
-
-        private void ButtonBuild_Click(object sender, RoutedEventArgs e)
-        {
-            // Generating points
+            // Generate points for graph
             var points = new List<Point>();
             for (var connectivity = 1; connectivity <= 100; connectivity += 5)
             {
-                var possibility = 0;
+                var possibility = .0;
                 for (var testNumber = 0; testNumber < TestsCount; testNumber++)
                     if (_schedule.CheckConflict(Convert.ToInt16(SliderSize.Value), connectivity))
                         possibility++;
                 possibility /= TestsCount;
-                points.Add(new Point(connectivity * ImageGraph.Width * .01, ImageGraph.Height - possibility * ImageGraph.Height));
+                points.Add(new Point((int)(connectivity * CanvasGraph.RenderSize.Width * .01), (int)(CanvasGraph.RenderSize.Height - possibility * CanvasGraph.RenderSize.Height)));
             }
+
+            // Draw graph
+
+
+            var b = new SolidColorBrush(_colors[_colorIdx++ % _colors.Count]);
+            for (var i = 0; i < points.Count - 1; i++)
+            {
+                var line = new Line
+                {
+                    Stroke = b,
+                    X1 = points[i].X,
+                    X2 = points[i + 1].X,
+                    Y1 = points[i].Y,
+                    Y2 = points[i + 1].Y,
+                    StrokeThickness = 2
+                };
+
+                CanvasGraph.Children.Add(line);
+            }
+        }
+
+        /// <summary>
+        /// Creates a Color from alpha, _hue, saturation and brightness.
+        /// </summary>
+        /// <param name="alpha">The alpha channel value.</param>
+        /// <param name="hue">The _hue value.</param>
+        /// <param name="saturation">The saturation value.</param>
+        /// <param name="brightness">The brightness value.</param>
+        /// <returns>A Color with the given values.</returns>
+        private void FromAhsb(byte alpha, float hue, float saturation, float brightness)
+        {
+        }
+
+        private void ButtonClear_Click(object sender, RoutedEventArgs e)
+        {
+            _colorIdx = 0;
+            CanvasGraph.Children.Clear();
+        }
+
+        private void CanvasGraph_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            _colorIdx = 0;
+            CanvasGraph.Children.Clear();
         }
     }
 }
+
--- T7_SSW_Lab4/Schedule.cs
+++ T7_SSW_Lab4/Schedule.cs
@@ -6,68 +6,81 @@ namespace T7_SSW_Lab4
 {
     public class Schedule
     {
-        public List<List<short>> Matrix;
+        private List<List<short>> _matrix;
         private readonly Random _rand = new Random();
 
-        public void GenerateMarix(int size, int connectivity)
+        // Generates random matrix with specified size and connectivity
+        private void GenerateMarix(int size, int connectivity)
         {
-            Matrix = new List<List<short>>();
+            _matrix = new List<List<short>>();
 
             for (var i = 0; i < size; i++)
             {
-                Matrix.Add(new List<short>(size));
+                _matrix.Add(new List<short>(size));
                 for (var j = 0; j < size; j++)
-                    Matrix[i].Add(0);
+                    _matrix[i].Add(0);
             }
 
-            for (var i = 0; i < connectivity; i++)
+
+            // Filling with ones
+            for (var i = 0; i < connectivity * size * size * .01; i++)
             {
                 while (true)
                 {
                     var x = _rand.Next(0, size);
                     var y = _rand.Next(0, size);
-                    if (Matrix[x][y] != 0) continue;
-                    Matrix[x][y] = 1;
+                    if (_matrix[x][y] != 0) continue;
+                    _matrix[x][y] = 1;
                     break;
                 }
             }
         }
 
+        // Removes specified row and column from matrix
+        // Addidtional for RemoveNeccesary
         private void RemoveRowCol(int row, int col)
         {
-            Matrix.RemoveAt(row);
-            foreach (var r in Matrix)
+            _matrix.RemoveAt(row);
+            foreach (var r in _matrix)
                 r.RemoveAt(col);
         }
 
+        // Removes nessesary assignments
         private void RemoveNeccesary()
         {
-            for (var i = 0; i < Matrix.Count; i++)
-                for (var j = 0; j < Matrix.Count; j++)
-                    if (Matrix[i][j] == 1)
+            for (var i = 0; i < _matrix.Count; i++)
+                for (var j = 0; j < _matrix.Count; j++)
+                    // If current cell is 1 and all others in row and column are zeros
+                    if (_matrix[i][j] == 1)
                     {
-                        var countZerosCol = Matrix.Count(t => t[j] == 0);
-                        if (countZerosCol != Matrix.Count - 1) continue;
+                        var countZerosCol = _matrix.Count(t => t[j] == 0);
+                        if (countZerosCol != _matrix.Count - 1) continue;
                         {
-                            var countZerosRow = Matrix.Where((t, j1) => Matrix[i][j1] == 0).Count();
-                            if (countZerosRow != Matrix.Count - 1) continue;
+                            var countZerosRow = _matrix.Where((t, j1) => _matrix[i][j1] == 0).Count();
+                            if (countZerosRow != _matrix.Count - 1) continue;
                             RemoveRowCol(i, j);
+
+                            // Do the same with lesser matrix
                             RemoveNeccesary();
                             return;
                         }
                     }
         }
 
+        // Swaps two rows in matrix
+        // Additional for MakeZeroMatrixRecursive
         private void SwapRows(int row1, int row2)
         {
-            var buf = Matrix[row1];
-            Matrix[row1] = Matrix[row2];
-            Matrix[row2] = buf;
+            var buf = _matrix[row1];
+            _matrix[row1] = _matrix[row2];
+            _matrix[row2] = buf;
         }
 
+        // Swaps two columns in matrix
+        // Additional for MakeZeroMatrixRecursive
         private void SwapCols(int col1, int col2)
         {
-            foreach (var t in Matrix)
+            foreach (var t in _matrix)
             {
                 var tmp = t[col1];
                 t[col1] = t[col2];
@@ -75,33 +88,20 @@ namespace T7_SSW_Lab4
             }
         }
 
-/*
-        private string MatrixToString()
-        {
-            var s = "";
-            foreach (var row in Matrix)
-            {
-                for (var j = 0; j < Matrix.Count; j++)
-                    s += $"{row[j],4}";
-                s += "\n";
-            }
-            return s;
-        }
-*/
-
+        // Tries to make zero matrix in top right corner
         private void MakeZeroMatrixRecursive(int offset)
         {
-            if (offset == Matrix.Count)
+            if (offset == _matrix.Count)
                 return;
 
+            // Find row with min number of zeros
             var imin = 0;
-            var min = Matrix.Count;
-            for (var i = offset; i < Matrix.Count; i++)
+            var min = _matrix.Count;
+            for (var i = offset; i < _matrix.Count; i++)
             {
-                //var currentOnes = Matrix.Where((t, j) => Matrix[i][j] == 1).Count();
                 var currentOnes = 0;
-                for (var j = offset; j < Matrix.Count; j++)
-                    if (Matrix[i][j] == 1)
+                for (var j = offset; j < _matrix.Count; j++)
+                    if (_matrix[i][j] == 1)
                         currentOnes++;
 
                 if (currentOnes >= min) continue;
@@ -109,23 +109,16 @@ namespace T7_SSW_Lab4
                 imin = i;
             }
 
-            //var s = MatrixToString();
-
+            // If found such row
             if (offset < imin)
                 SwapRows(offset, imin);
 
-            //var s1 = MatrixToString();
-
-            //var dialog = new MessageDialog(s + $"{offset,4} min row - {imin,4}\n" + s1);
-            ////var dialog = new MessageDialog($"{offset,4} min row - {imin, 4}");
-            //await dialog.ShowAsync();
-
-
+            // Find column with max number of ones where cell on "first" line is 1
             var maxInitialised = false;
             var jmax = 0;
             var max = 0;
-            for (var j = 0; j < Matrix.Count; j++)
-                if (Matrix[offset][j] == 1)
+            for (var j = 0; j < _matrix.Count; j++)
+                if (_matrix[offset][j] == 1)
                 {
                     if (!maxInitialised)
                     {
@@ -133,10 +126,9 @@ namespace T7_SSW_Lab4
                         maxInitialised = true;
                     }
 
-                    //var countOnes = Matrix.Count(t => t[j] == 1);
                     var countOnes = 0;
-                    for (var i = offset; i < Matrix.Count; i++)
-                        if (Matrix[i][j] == 1)
+                    for (var i = offset; i < _matrix.Count; i++)
+                        if (_matrix[i][j] == 1)
                             countOnes++;
 
 
@@ -145,58 +137,40 @@ namespace T7_SSW_Lab4
                     jmax = j;
                 }
 
-            //var s2 = MatrixToString();
-
-            //var dialog1 = new MessageDialog($"{offset,4} max col - {jmax,4}");
-            //dialog1.ShowAsync();
-
+            // If found such row
             if (offset < jmax)
                 SwapCols(offset, jmax);
 
-            //var s3 = MatrixToString();
-            //var dialog1 = new MessageDialog(s2 + $"{offset,4} max col - {jmax,4}\n" + s3);
-            ////var dialog = new MessageDialog($"{offset,4} min row - {imin, 4}");
-            //await dialog1.ShowAsync();
-
-            //await Task.Delay(100000);
-
+            // Do the same with lesser matrix
             // ReSharper disable once TailRecursiveCall
             MakeZeroMatrixRecursive(offset + 1);
         }
 
-        public void TransformMatrix()
+        // Checks matrix for conflict assignments
+        private bool CheckConflict()
         {
-            // Look for nessesary solutions
-            RemoveNeccesary();
-
-
-            MakeZeroMatrixRecursive(0);
-
-        }
-
-        public bool CheckConflict()
-        {
-            for (var i = 0; i < Matrix.Count; i++)
-                if (Matrix[i][i] == 1)
+            for (var i = 0; i < _matrix.Count; i++)
+                if (_matrix[i][i] == 1)
                 {
                     var zerosNumber = 0;
-                    for (var i1 = 0; i1 <=i; i1++)
-                        for (var j1 = i; j1 < Matrix.Count; j1++)
-                            if (Matrix[i1][j1] == 0)
+                    for (var i1 = 0; i1 <= i; i1++)
+                        for (var j1 = i; j1 < _matrix.Count; j1++)
+                            if (_matrix[i1][j1] == 0)
                                 zerosNumber++;
-                    if (zerosNumber == (i + 1)*(Matrix.Count - i) - 1)
+                    if (zerosNumber == (i + 1) * (_matrix.Count - i) - 1)
                         return true;
                 }
 
             return false;
         }
 
+        // Generates and checks matrix for conflict assignments
         public bool CheckConflict(int size, int connectivity)
         {
             GenerateMarix(size, connectivity);
-            TransformMatrix();
+            RemoveNeccesary();
+            MakeZeroMatrixRecursive(0);
             return CheckConflict();
         }
     }
 }
-
